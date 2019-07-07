812446507befe11037bcd7e3bd19e54713a70640
hamza enaime <hamza.enaime@outlook.com>
2019-07-07 14:50:43 +0100

    change jdk version in pom.xml

--- src/main/java/com/pfproject/api/config/CorsFilter.java
+++ src/main/java/com/pfproject/api/config/CorsFilter.java
@@ -12,7 +12,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-
 @Component
 public class CorsFilter implements Filter {
 
@@ -22,17 +21,16 @@ public class CorsFilter implements Filter {
     }
 
     @Override
-    public void doFilter(final ServletRequest req, final ServletResponse res,
-                         final FilterChain chain) throws IOException, ServletException {
+    public void doFilter(final ServletRequest req, final ServletResponse res, final FilterChain chain)
+            throws IOException, ServletException {
         final HttpServletResponse response = (HttpServletResponse) res;
-        response.setHeader("Access-Control-Allow-Origin", "*");
-        response.setHeader("Access-Control-Allow-Methods",
-                "POST, GET, PUT, OPTIONS, DELETE, PATCH");
+        final HttpServletRequest request = (HttpServletRequest) req;
+         response.setHeader("Access-Control-Allow-Origin", "*");
+
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE, PATCH");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers", "x-auth-token, Content-Type");
         response.setHeader("Access-Control-Expose-Headers", "x-auth-token, Content-Type");
-
-        final HttpServletRequest request = (HttpServletRequest) req;
 
         if (request.getMethod().equals("OPTIONS")) {
             try {
