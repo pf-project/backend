d85a13629805a45f14379a7883c07e9fb2b76a4f
taoufik <taoufik.ajaanit.1@gmail.com>
2019-09-18 15:57:10 +0100

    add repositiory anotation

--- src/main/java/com/pfproject/api/repository/comptabilite/donneedebase/comptebancaire/CompteBancaireRepository.java
+++ src/main/java/com/pfproject/api/repository/comptabilite/donneedebase/comptebancaire/CompteBancaireRepository.java
@@ -3,9 +3,12 @@ package com.pfproject.api.repository.comptabilite.donneedebase.comptebancaire;
 import com.pfproject.api.model.comptabilite.donneedebase.comptebancaire.CompteBancaire;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+
+@Repository
 public interface CompteBancaireRepository extends MongoRepository<CompteBancaire, String> {
     CompteBancaire findByCode(final String code);
 
